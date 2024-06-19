120d5c8bae586ccbc8052ed48a141fa4d846fb8e
Wongso Wijaya <zarchxxx@gmail.com>
2024-06-19 13:48:06 +0700

    feat: Link for book information

--- client/src/components/bookList.tsx
+++ client/src/components/bookList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Card, CardDescription } from "./ui/card";
 
 const BookList = ({
@@ -5,32 +6,36 @@ const BookList = ({
   name,
   className,
   list,
+  link,
 }: {
   image: string;
   name: string;
   className?: string;
   list?: boolean;
+  link: string;
 }) => {
   return (
-    <Card
-      className={`group cursor-pointer ${
-        !list ? "w-[96px] md:w-[148px]" : "aspect-[9/15] max-w-[300px]"
-      } ${className}`}
-    >
-      <div
-        className={`flex flex-col items-center p-1 ${list && "aspect-[2/3]"}`}
+    <Link to={link}>
+      <Card
+        className={`group cursor-pointer ${
+          !list ? "w-[96px] md:w-[148px]" : "aspect-[9/15] max-w-[300px]"
+        } ${className}`}
       >
-        <img
-          src={`/${image}`}
-          className={`mb-3 saturate-[.7] group-hover:saturate-100 ${
-            !list ? "max-h-[120px] md:max-h-[190px]" : "h-full"
-          } rounded`}
-        />
-        <CardDescription className="text-center group-hover:text-zinc-800 line-clamp-1 px-1">
-          {name}
-        </CardDescription>
-      </div>
-    </Card>
+        <div
+          className={`flex flex-col items-center p-1 ${list && "aspect-[2/3]"}`}
+        >
+          <img
+            src={`/${image}`}
+            className={`mb-3 saturate-[.7] group-hover:saturate-100 ${
+              !list ? "max-h-[120px] md:max-h-[190px]" : "h-full"
+            } rounded`}
+          />
+          <CardDescription className="text-center group-hover:text-zinc-800 line-clamp-1 px-1">
+            {name}
+          </CardDescription>
+        </div>
+      </Card>
+    </Link>
   );
 };
 
