628806db672b4bae9c79ccc23ab8d8ee5356d5cb
Wongso Wijaya <zarchxxx@gmail.com>
2024-06-19 13:47:41 +0700

    feat: fake api

---
+++ client/src/__fakeapi__/data.ts
@@ -0,0 +1,43 @@
+const bookDatas: BookDatas[] = [
+  {
+    id: 1,
+    image: "bumi.jpg",
+    name: "Bumi",
+    author: "Tere Liye",
+    stock: 0,
+    tag: ["bumi", "tere", "liye", "tereliye", "earth"],
+    type: "Literatur",
+  },
+  {
+    id: 2,
+    image: "bulan.jpg",
+    name: "Bulan",
+    author: "Tere Liye",
+    stock: 0,
+    tag: ["bulan", "tere", "liye", "tereliye", "moon"],
+    type: "Literatur",
+  },
+  {
+    id: 3,
+    image: "bintang.jpg",
+    name: "Bintang",
+    author: "Tere Liye",
+    stock: 0,
+    tag: ["bintang", "tere", "liye", "tereliye", "star"],
+    type: "Literatur",
+  },
+  {
+    id: 4,
+    image: "Gagal-Menjadi-Manusia.jpg",
+    name: "Gagal Menjadi Manusia",
+    author: "Dazai Osamu",
+    desc: `Gagal Menjadi Manusia adalah sebuah novel karangan Osamu Dazai yang menjadi novel terbaik kedua di Jepang setelah Kokoro karya Natsume Soseki. Arti dari judulnya kerap dibahas oleh Donald Keene dalam kata pengantar untuk terjemahan bahasa Inggris, seperti "Didiskualifikasi Menjadi Manusia".`,
+    stock: 5,
+    tag: ["gagal", "menjadi", "manusia", "osamudazai", "dazaiosamu"],
+    genre: "Novel Pendek",
+    year: 1948,
+    type: "Literatur",
+  },
+];
+
+export default bookDatas;
