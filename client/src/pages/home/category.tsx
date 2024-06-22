474d15d0208dd7023ed408f5eb8b99c4c4f70a99
Wongso Wijaya <zarchxxx@gmail.com>
2024-06-22 18:15:00 +0700

    up: padding bottom from scroll & link

--- client/src/pages/home/category.tsx
+++ client/src/pages/home/category.tsx
@@ -1,33 +1,13 @@
+import categoryData from "@/categoryData";
 import CardCategory from "@/components/cardCategory";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const Category = () => {
-  const categoryData = [
-    {
-      icons: "/assets/category/books.png",
-      desc: "Literatur",
-    },
-    {
-      icons: "/assets/category/chemical.png",
-      desc: "Komputer & Program",
-    },
-    { icons: "/assets/category/memory.png", desc: "Psikologi & Filosofi" },
-    {
-      icons: "/assets/category/quill.png",
-      desc: "Karya Seni & Hiburan",
-    },
-    {
-      icons: "/assets/category/translation.png",
-      desc: "Bahasa",
-    },
-    { icons: "/assets/category/returntothepast.png", desc: "Sejarah" },
-    { icons: "/assets/category/math.png", desc: "Matematika & Sains" },
-  ];
   return (
     <section>
       <h3>Kategori buku</h3>
       <p className="text-zinc-500">Pilih kategori buku kesukaanmu</p>
-      <ScrollArea className="mt-5 h-full">
+      <ScrollArea className="mt-5 h-full pb-3">
         <section className="flex h-full">
           {categoryData.map((e, i) => (
             <CardCategory
@@ -35,6 +15,7 @@ const Category = () => {
               desc={e.desc}
               className="mx-2 md:mx-5"
               key={i}
+              link={`/types/${e.desc.split(" ").join("-")}`}
             />
           ))}
         </section>
