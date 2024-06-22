043ba7f6e2d01fce45552128c64853d33d1c1177
Wongso Wijaya <zarchxxx@gmail.com>
2024-06-22 18:14:34 +0700

    up: add link tag

--- client/src/components/cardCategory.tsx
+++ client/src/components/cardCategory.tsx
@@ -1,26 +1,34 @@
+import { Link } from "react-router-dom";
 import { Card, CardHeader, CardDescription } from "./ui/card";
 
 const CardCategory = ({
   icons,
   desc,
   className,
+  link,
 }: {
   icons: string;
   desc: string;
   className?: string;
+  link: string;
 }) => {
   return (
-    <Card className={"group cursor-pointer w-[96px] md:w-[148px] " + className}>
-      <CardHeader className="flex flex-col items-center">
-        <img
-          src={icons}
-          className="mb-3 saturate-[.7] group-hover:saturate-100 lg:min-w-[75px]"
-        />
-        <CardDescription className="text-center group-hover:text-zinc-800">
-          <p className="max-sm:truncate max-sm:w-20">{desc}</p>
-        </CardDescription>
-      </CardHeader>
-    </Card>
+    <Link
+      to={link}
+      className={`${className} w-[96px] md:w-[148px] group cursor-pointer`}
+    >
+      <Card className="h-full">
+        <CardHeader className="flex flex-col items-center">
+          <img
+            src={icons}
+            className="mb-3 saturate-[.7] group-hover:saturate-100 lg:min-w-[75px]"
+          />
+          <CardDescription className="text-center group-hover:text-zinc-800">
+            <p className="max-sm:line-clamp-1">{desc}</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 };
 
