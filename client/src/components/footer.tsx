9ba48bb114ee09ce425027dc0c50614bd5639dbc
Wongso Wijaya <zarchxxx@gmail.com>
2024-06-29 14:05:20 +0700

    rmv: text to white

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-green-900 text-yellow-50 p-5">
+    <footer className="w-full bg-green-900 text-white p-5">
       <div className="flex items-center justify-center">
         <TbBooks size={30} className="mr-1 md:mr-3 max-md:w-6 max-md:h-6" />
         <h5 className="max-md:text-sm">Perpustakaan Mayang Mengurai</h5>
