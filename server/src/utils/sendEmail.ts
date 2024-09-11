b7faca91c57c33ada6a891d476d0261326d68080
zackdesu <zarchxxx@gmail.com>
2024-09-11 09:14:08 +0700

    feat: sendEmail

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -2,37 +2,33 @@ import nodemailer from "nodemailer";
 import { throwError } from "./throwError";
 
 const sendEmail = async (
-  email: string,
+  to: string,
   otp: number,
-  subject = "Atur ulang kata sandi"
+  subject = "Atur ulang kata sandi",
+  text = `Code OTP mu adalah ${otp}`
 ) => {
-  try {
-    const user = process.env.EMAIL;
-    const pass = process.env.PASSWORD;
+  const user = process.env.EMAIL;
+  const pass = process.env.PASSWORD;
 
-    if (!user || !pass)
-      return throwError(500, "Email & Password is not found!");
+  if (!user || !pass) return throwError(500, "Email & Password is not found!");
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user,
-        pass,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user,
+      pass,
+    },
+  });
 
-    const mailOptions = {
-      from: `Perpustakaan Mayang Mengurai <${user}>`,
-      to: email,
-      subject,
-      text: `Code OTP mu adalah ${otp}`,
-    };
+  const mailOptions = {
+    from: `Perpustakaan Mayang Mengurai <${user}>`,
+    to,
+    subject,
+    text,
+  };
 
-    await transporter.sendMail(mailOptions);
-    console.log("Message sent successfully!");
-  } catch (error) {
-    console.log(error);
-  }
+  await transporter.sendMail(mailOptions);
+  console.log("Message sent successfully!");
 };
 
 export default sendEmail;
