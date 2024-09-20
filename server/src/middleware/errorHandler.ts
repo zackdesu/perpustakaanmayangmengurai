ec6849ab4497fa3c684a3d509fadacdb4efbdfd8
zackdesu <zarchxxx@gmail.com>
2024-09-20 12:40:24 +0700

    up: catch zodError

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,18 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 const errorHandler = (
-  err: { statusCode: number; message?: string },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  err: any,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
-  const statusCode = err.statusCode || 500;
+  const statusCode = err instanceof ZodError ? 400 : err.statusCode || 500;
 
   const message =
-    process.env.NODE_ENV === "development"
-      ? err.message
-      : "Internal Server Error!";
-
+    statusCode === 500
+      ? process.env.NODE_ENV === "development"
+        ? err.message
+        : "Internal Server Error!"
+      : err.message || err.errors.forEach((err: unknown) => err);
   console.log(message);
 
   return res.status(statusCode).json({
