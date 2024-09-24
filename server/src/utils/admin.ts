cbe8a87389bdef8e3cd2daf09d96656b5aa2aae0
zackdesu <zarchxxx@gmail.com>
2024-09-24 10:01:28 +0700

    update admin

--- server/src/utils/admin.ts
+++ server/src/utils/admin.ts
@@ -1,13 +1,13 @@
 import prisma from "./db";
 
-prisma.acc
-  .update({ where: { username: "admin" }, data: { role: "ADMIN" } })
-  .then(console.log);
+// prisma.acc
+//   .update({ where: { username: "admin" }, data: { role: "ADMIN" } })
+//   .then(console.log);
 // prisma.loan.findMany().then((res) => {
 //   console.log(res);
 // });
 
-prisma.book.findMany().then(console.log);
+// prisma.book.findMany().then(console.log);
 // prisma.acc.delete({ where: { username: "zackdesuu" } }).then(console.log);
 // prisma.loan.deleteMany().then(console.log);
 // prisma.book
@@ -20,3 +20,5 @@ prisma.book.findMany().then(console.log);
 // prisma.book
 //   .findFirst({ where: { id: "9ab0cd6d-f916-4d6b-83d0-8584a80f30d2" } })
 //   .then(console.log);
+
+prisma.book.findMany().then(console.log);
