b9203f40d1c2ee86970213b239e2c90837779333
zackdesu <zarchxxx@gmail.com>
2024-09-28 15:16:43 +0700

    err unknown and using instance

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,27 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
+import logger from "../utils/logger";
+import HttpError from "../utils/HttpError";
 const errorHandler = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  err: any,
-  req: Request,
+  err: unknown,
+  _req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction
+  _next: NextFunction
 ) => {
-  const statusCode = err instanceof ZodError ? 400 : err.statusCode || 500;
+  let statusCode = 500;
+  let message = "Internal Server Error";
 
-  const message =
-    statusCode === 500
-      ? process.env.NODE_ENV === "development"
-        ? (err.errors &&
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            err.errors.map((err: any) => err.message).join(", ")) ||
-          err.message
-        : "Internal Server Error!"
-      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (err.errors && err.errors.map((err: any) => err.message).join(", ")) ||
-        err.message;
-  console.log(message);
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = err.message;
+  } else if (err instanceof HttpError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  logger.error(message);
 
   return res.status(statusCode).json({
     error: message,
