828b366e7aab904cfcab6f84b8581e066e20996c
zackdesu <zarchxxx@gmail.com>
2024-10-12 08:21:42 +0700

    explain error more clearly

--- server/src/middleware/isAdmin.ts
+++ server/src/middleware/isAdmin.ts
@@ -5,9 +5,8 @@ import HttpError from "../utils/HttpError";
 const isAdmin = (req: IRequest, _res: Response, next: NextFunction) => {
   if (!req.payload) throw new HttpError(500, "req.payload undefined!");
   const { role } = req.payload;
-
   if (role !== "ADMIN")
-    throw new HttpError(403, "Penghapusan buku harus di lakukan oleh admin!");
+    throw new HttpError(403, "Hanya Admin yang dapat mengakses!");
 
   next();
 };
